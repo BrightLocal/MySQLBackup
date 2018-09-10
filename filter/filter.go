6b29dc64c3e99e5bdda27b3ba4ff09e078061713
Sergey Mudrik <sergey.mudrik@gmail.com>
2018-09-10 14:46:27 +0300

    First implementation of apply filter

--- filter/filter.go
+++ filter/filter.go
@@ -36,7 +36,7 @@ type Expr struct {
 }
 
 // NewFilterSet returns new filters for expression:
-// table_name(field == "val"),table02(field02 != "val2" && field03 == 123)
+// table_name(field == "val"),table02(field02 != "val2" AND field03 == 123)
 func NewFilterSet(expression string) (FilterSet, error) {
 	result := map[string]*Filter{}
 
@@ -71,5 +71,68 @@ func (f *Filter) getTableName() (string, error) {
 }
 
 func (f *Filter) Passes(data map[string]interface{}) bool {
-	return true
+	if f.expr.Type != OperandExpression || f.expr.X == nil || f.expr.Y == nil {
+		// TODO: return error?
+		return false
+	}
+
+	return f.expr.eval(data)
+}
+
+func (expr Expr) eval(data map[string]interface{}) bool {
+	if expr.Type == OperandExpression {
+		if expr.X == nil || expr.Y == nil {
+			// TODO: error in expression
+			return false
+		}
+
+		switch expr.Op {
+		case OpEq, OpNe:
+
+			var x, y interface{}
+
+			switch expr.X.Type {
+			case OperandField:
+				// TODO: check exists field in map
+				x = data[expr.X.Name]
+			case OperandValue:
+				x = expr.X.Value
+			default:
+				// TODO error
+				return false
+			}
+			switch expr.Y.Type {
+			case OperandField:
+				// TODO: check exists field in map
+				y = data[expr.Y.Name]
+			case OperandValue:
+				y = expr.Y.Value
+			default:
+				// TODO error
+				return false
+			}
+
+			if expr.Op == OpEq {
+				return x == y
+			}
+
+			return x != y
+
+		case OpAnd, OpOr:
+			if expr.X.Type == OperandExpression && expr.Y.Type == OperandExpression {
+				if expr.Op == OpAnd {
+					return expr.X.eval(data) && expr.Y.eval(data)
+				}
+				return expr.X.eval(data) || expr.Y.eval(data)
+
+			} else {
+				// TODO error
+				return false
+			}
+		}
+	} else {
+		return false
+	}
+
+	return false
 }
